ab334428ccdfe95d2ef7be6e33b97c8ca8a4fbfe
O R A C L E <122223761+TheUncertainOracle@users.noreply.github.com>
2023-09-11 21:20:17 +0530

    Update CRICKET.go

--- CRICKET.go
+++ CRICKET.go
@@ -5,34 +5,69 @@ import (
 	"math/rand"
 )
 
-// try to utilise all that you have learnt so far
-
 func main() {
 	var name string
+	var empty string
+	fmt.Println()
+	fmt.Println("---------------------------")
+	fmt.Println("| WELCOME TO CRICKET CLI! |")
+	fmt.Println("---------------------------\n")
 
-	fmt.Printf("SWITCHCASES - A Game Of CRICKET\n")
-	fmt.Println("Enter your name, batsman!")
+	fmt.Println("\nEnter your name, batsman!")
 	fmt.Scanln(&name)
 
-	possibleRuns := []int{2, 4, 6}                //Slice of int used to store the runs
-	runsByBatsman := rand.Intn(len(possibleRuns)) //Should be 6 but since last digit ignored in range, 7 is used
+	possibleRuns := []int{1, 2, 3, 4, 5, 6}
+	totalRun := 0 //Slice of int used to store the runs
+	fmt.Printf("\n\nNice to have you on the team, %v! You're our operning player.\n", name)
+	fmt.Println("RULE: You will be playing until you are declared 'Out' by the bowler.\nAre you ready?")
+	fmt.Scanln(&empty)
+	fmt.Println("\n----------------------------------------------")
 
-	fmt.Println("\nLasith Malinga bowls a viscous ball and...\n----------------------------------------------")
-	fmt.Printf("%v hits the ball hardly with his bat!\n", name)
+	for {
+		fmt.Println("\nChoose batting style\n- HS for Helicopter shot\n- SW for Side-Sweep\n- UC for Upper-Cut\n")
+		var battingStyle string
+		fmt.Scanln(&battingStyle)
 
-	// for {
-	switch runsByBatsman {
-	case 2:
-		fmt.Println("The 2 batmen on the ground are running b/w the wickets and have just made\n--2 RUNS--\n")
-	case 4:
-		fmt.Printf("A short-shot off the field and %v effortlessly scores a sweeping 4\n--RUNS--!\n", name)
-	case 6:
-		fmt.Printf("A long shot sends the ball flying out of the stadium! What a shot by %v as he scores\n--6 RUNS--!", name)
-	default:
-		fmt.Println("OH! What a tragedy! %v has been caught out... It's a game over for him!")
-		break
+		battingStyles := [3]string{"HS", "SW", "UC"}
+
+		if battingStyle == battingStyles[0] {
+			fmt.Printf("You Chose Helicopter shot!")
+		} else if battingStyle == battingStyles[1] {
+			fmt.Printf("You Chose Side-Sweep!")
+		} else if battingStyle == battingStyles[2] {
+			fmt.Printf("You Chose Upper-Cut!")
+		} else {
+			fmt.Printf("Invalid choice - try again")
+			continue
+		}
+
+		fmt.Printf("\nLasith Malinga bowls a viscous ball and %v hits the ball hardly with his bat!\n", name)
+		fmt.Printf("\n----------------------------------------------\n")
+
+		runsByBatsman := rand.Intn(len(possibleRuns))
+		totalRun += runsByBatsman
+
+		switch runsByBatsman {
+		case 1:
+			fmt.Println("Run by Runs\n1 RUN\nTotal runs: ", totalRun)
+		case 2:
+			fmt.Println("Run by Runs\n2 RUN\nTotal runs: ", totalRun)
+		case 3:
+			fmt.Println("Run by Runs\n3 RUN\nTotal runs: ", totalRun)
+		case 4:
+			fmt.Printf("A sweeping shot through the boundary!\n4 RUN\nTotal runs: ", totalRun)
+		case 5:
+			fmt.Println("Run by Runs\n5 RUN\nTotal runs: ", totalRun)
+		case 6:
+			fmt.Printf("%v sends the ball flying out of the stadium! What a shot!\n6 RUN\nTotal runs: ", name, totalRun)
+		default:
+			fmt.Printf("OH! What a tragedy! %v has been caught out... It's a game over for him!\n", name)
+			goto RESULT
+		}
 	}
-	// }
-}
 
-// add trigger such as play lower cut/uppercut/sideswee/etc
+RESULT:
+	fmt.Printf("Total runs made by %v were: %d\nBetter luck next time!\n", name, totalRun)
+	// fmt.Printf("Total balls played were: %d\n", name, totalRun)
+
+}
